89f1df819620aecaf08bb57ce87d45dee2b1599a
altamashk1252 <31143974+altamashk1252@users.noreply.github.com>
2025-10-06 11:51:37 +0400

    Update User.js
    added admission year,category,dob,mobile,guardian mobile

--- models/User.js
+++ models/User.js
@@ -1,6 +1,5 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('./index');
-
 const User = sequelize.define('User', {
   name: { type: DataTypes.STRING, allowNull: false },
   email: { type: DataTypes.STRING, allowNull: false, unique: true },
@@ -9,6 +8,12 @@ const User = sequelize.define('User', {
   subject: { type: DataTypes.STRING },
   program: { type: DataTypes.STRING },
   year: { type: DataTypes.INTEGER },
+  admissionYear: { type: DataTypes.INTEGER },
+  category: { type: DataTypes.STRING },
+  dob: { type: DataTypes.DATEONLY },
+  mobile: { type: DataTypes.STRING },
+  guardianMobile: { type: DataTypes.STRING },
 });
+
 
 module.exports = User;
